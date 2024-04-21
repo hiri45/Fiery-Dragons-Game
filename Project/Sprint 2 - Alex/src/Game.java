31c81b71f1f337a5e6ef5071a8701e1c471e808f
Alex Ung <aung0008@student.monash.edu>
2024-04-21 12:55:56 +1000

    setting up of packages and classes for development

---
+++ Project/Sprint 2 - Alex/src/Game.java
@@ -0,0 +1,15 @@
+package src;
+
+import src.board.BoardArray;
+import src.board.DragonCardArray;
+import src.utils.TurnManager;
+
+public class Game{
+
+    public static void main(String[] args) {
+        BoardArray boardArray = new BoardArray();
+        DragonCardArray dragonCardArray = new DragonCardArray();
+        TurnManager = new TurnManager(playerCount);
+    }
+
+}
---
+++ Project/Sprint 2 - Alex/src/board/BoardArray.java
@@ -0,0 +1,4 @@
+package src.board;
+
+public class BoardArray {
+}
