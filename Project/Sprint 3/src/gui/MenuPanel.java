1b98fd68958c832437da8167ed13c3d3c0f98953
Hirun <himeshahettigoda@gmail.com>
2024-06-02 16:05:44 +1000

    menu screen created which goes to game panel when start button pressed

---
+++ Project/Sprint 3/src/gui/MenuPanel.java
@@ -0,0 +1,64 @@
+package src.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuPanel extends JPanel {
+    private static MenuPanel instance;
+    JLabel titleLabel;
+    JPanel startButtonPanel;
+    JButton startButton;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+    private JFrame frame;
+
+    private MenuPanel(JFrame frame) {
+        this.frame = frame;
+        this.setBounds(150, 100, 600, 150);
+        this.setBackground(Color.black);
+        titleLabel = new JLabel("Fiery Dragons");
+        titleLabel.setForeground(Color.white);
+        titleLabel.setFont(titleFont);
+
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(350, 400, 200, 100);
+        startButtonPanel.setBackground(Color.black);
+
+        startButton = new JButton("START");
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.white);
+        startButton.setFont(normalFont);
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startGame();
+            }
+        });
+
+        this.add(titleLabel);
+        startButtonPanel.add(startButton);
+
+    }
+
+    public static MenuPanel getInstance(JFrame frame) {
+        if (instance == null) {
+            instance = new MenuPanel(frame);
+        }
+        return instance;
+    }
+
+    private void startGame() {
+        frame.getContentPane().removeAll();
+        WindowPanel gameWindow = WindowPanel.getInstance();
+        JScrollPane scrollPane = new JScrollPane(gameWindow);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+        // Add the JScrollPane to the frame
+        frame.add(scrollPane); // Add the scroll pane, not the game panel directly*/
+        frame.revalidate();
+        frame.repaint();
+    }
+}
