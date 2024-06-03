8ffcc2d2c1830238834fdb938748fdb0073ddf08
Romal Patel <rpat0035@student.monash.edu>
2024-06-03 19:45:13 +1000

    added BeaverWizardCard class to board package

---
+++ Project/Sprint 3/src/board/BeaverWizardCard.java
@@ -0,0 +1,33 @@
+package src.board;
+
+import src.actors.DragonToken;
+import src.utils.PlayerManager;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Random;
+
+public class BeaverWizardCard extends JPanel {
+    private final ImageIcon front = new ImageIcon(Objects.requireNonNull(DragonCard.class.getResource("/src/Images/dragon card resize.png")));
+    private ArrayList<DragonToken> used = new ArrayList<DragonToken>();
+
+
+
+
+
+    @Override
+    /**
+     * Paints the component with a custom background.
+     * @param g the Graphics object to protect
+     */
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Color brownBackground = new Color(51, 0, 0);
+        g.setColor(brownBackground);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+    }
+}
